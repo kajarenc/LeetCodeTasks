5747c15861d4ec06650bedbef867d25a14a17ef5
Karen Javadyan <kajarenc@gmail.com>
2019-09-06 17:56:27 +0400

    remove dublicates from sorted linked list solved

---
+++ remove-dublicates-sorted-linked-lilst.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+
+        result = dummyHead
+        current = head
+
+        while current:
+            val = current.val
+            new_node = ListNode(val)
+            result.next = new_node
+            result = result.next
+
+            while current.next and current.next.val == val:
+                current = current.next
+            current = current.next
+
+        return dummyHead.next
